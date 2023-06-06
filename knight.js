0e3d601930e8870ca022b7d15d10a01474a9a63f
Josh <joshh2201@gmail.com>
2023-06-06 18:11:34 -0400

    Add method to tree to find legal knight moves
    The method accepts a Position factory as input and iterates through all
    possible knight moves to check if they are on the chess board.

--- knight.js
+++ knight.js
@@ -1,1 +1,28 @@
-const Move()
+const Position = (data = [0, 0], children = []) => ({ data, children });
+
+const Tree = () => {
+  const generateMoves = (position) => {
+    const row = position.data[0];
+    const col = position.data[1];
+    const legalMoves = [];
+    const knightMoves = [
+      [-2, -1],
+      [-2, 1],
+      [2, -1],
+      [2, 1],
+      [1, -2],
+      [1, 2],
+      [-1, -2],
+      [-1, 2],
+    ];
+    knightMoves.forEach((move) => {
+      const nextRow = row + move[0];
+      const nextCol = col + move[1];
+      if (nextRow >= 0 && nextRow <= 7 && nextCol >= 0 && nextCol <= 7) {
+        legalMoves.push([nextRow, nextCol]);
+      }
+    });
+    return legalMoves;
+  };
+  return { generateMoves };
+};
